b03735354bf0224e8b1c98fb7e72bacf8c3f041f
Sevkiscap <63850097+Sevkiscap@users.noreply.github.com>
2020-11-29 10:26:06 -0700

    first commit

---
+++ SceneLoader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] int waitTime = 4;
+    int sceneIndex;
+    void Start()
+    {
+       
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if(sceneIndex == 0)
+        {
+            StartCoroutine(SplashSceneCoroutine());
+        }       
+    }
+
+    IEnumerator SplashSceneCoroutine()
+    {
+        yield return new WaitForSeconds(4);
+        LoadNextScene();
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(sceneIndex + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
